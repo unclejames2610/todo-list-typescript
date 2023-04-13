fa6274a0b1c26a629f7c3cd63cbd0b3c7abc69ae
unclejames2610 <udealaebuka@gmail.com>
2023-04-13 13:18:22 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { FC } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="input-container">
+          <input type="text" placeholder="Task..." />
+          <input type="number" placeholder="Deadline (in days)" />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
