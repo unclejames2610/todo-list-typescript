11279ba9872f355f38ecf9284d0e0d832e2320e6
unclejames2610 <udealaebuka@gmail.com>
2023-04-13 15:23:51 +0100

    done with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,14 @@ const App: FC = () => {
     setDeadline(0);
   };
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
   return (
     <div className="App">
       <div className="header">
@@ -46,7 +54,7 @@ const App: FC = () => {
       </div>
       <div className="todoList">
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} />;
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
       </div>
     </div>
--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -3,15 +3,23 @@ import { ITask } from "../Interfaces";
 
 interface Props {
   task: ITask;
+  completeTask(taskNameToDelete: string): void;
 }
 
-const TodoTask = ({ task }: Props) => {
+const TodoTask = ({ task, completeTask }: Props) => {
   return (
     <div className="task">
       <div className="content">
         <span>{task.taskName}</span>
         <span>{task.deadline}</span>
       </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
     </div>
   );
 };
